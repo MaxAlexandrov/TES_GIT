042b3a347998a79e7d816939a107a0482845dbfe
Maksim Aleksandrov <maximalexandrov87@gmail.com>
2025-07-22 07:02:45 +0500

    INDEX-4: validation2

--- MergeRequest.java
+++ MergeRequest.java
@@ -2,9 +2,13 @@ class MergeRequest {
     public static void main(String[] args) {
         System.out.println("Hello World 22222");
         VALIDATION();
+        VALIDATION2();
     }
 
     public static void VALIDATION() {
+        System.out.println("VALID");
+    }
+    public static void VALIDATION2() {
         System.out.println("VALID");
     }
 }
